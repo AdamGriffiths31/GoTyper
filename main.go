d070ceae0fecb77f2cc4e604e0f9db97d09ee83f
Adam Griffiths <adam.j.griffiths@outlook.com>
2023-05-06 15:38:59 +0100

    Added random word mode

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"flag"
 	"time"
 
+	randomtext "github.com/AdamGriffiths31/Typing/randomText"
 	"github.com/AdamGriffiths31/Typing/types"
 	"github.com/charmbracelet/bubbles/progress"
 	"github.com/charmbracelet/bubbles/stopwatch"
@@ -11,9 +13,25 @@ import (
 )
 
 func main() {
+	mode := flag.String("mode", "", "Sets the mode of the program")
+	flag.Parse()
+
+	var text string
+
+	switch *mode {
+	case "random":
+		text = randomtext.GenerateText()
+	default:
+		text = "the quick brown fox jumps over the lazy dog"
+	}
+
+	execute(text)
+}
+
+func execute(text string) {
 	bar := progress.NewModel()
 	program := tea.NewProgram(types.Model{
-		Text:               []rune("the quick brown fox jumps over the lazy dog"),
+		Text:               []rune(text),
 		Progress:           &bar,
 		CompletedTextStyle: lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color("#00FF00")),
 		NextCharStyle:      lipgloss.NewStyle().Underline(true),
---
+++ randomText/randomText.go
@@ -0,0 +1,61 @@
+package randomtext
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"math/rand"
+	"net/http"
+	"strings"
+	"time"
+)
+
+type Response struct {
+	Words []string `json:"words"`
+}
+
+func GenerateText() string {
+	words, err := getRandomWords()
+	if err != nil {
+		return "Unable to generate random words"
+	}
+	words = pickRandomWords(words)
+
+	return strings.Join(words, " ")
+}
+
+func getRandomWords() ([]string, error) {
+	response, err := http.Get("https://monkeytype.com/languages/english_1k.json")
+	if err != nil {
+		return nil, err
+	}
+
+	defer response.Body.Close()
+
+	bodyBytes, err := io.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(bodyBytes) == 0 {
+		return nil, fmt.Errorf("body was empty")
+	}
+
+	var returnData Response
+	err = json.Unmarshal(bodyBytes, &returnData)
+	if err != nil {
+		return nil, err
+	}
+
+	return returnData.Words, nil
+}
+
+func pickRandomWords(words []string) []string {
+	var returnData []string
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+	for _, i := range r.Perm(10) {
+		returnData = append(returnData, words[i])
+	}
+
+	return returnData
+}
